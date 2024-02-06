0f02e61aee48a6ca257736ea43fec508ec1cf976
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-06 12:44:01 +0100

    Implement destroy project

--- app/Http/Controllers/Admin/ProjectsController.php
+++ app/Http/Controllers/Admin/ProjectsController.php
@@ -92,8 +92,10 @@ class ProjectsController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Project $project)
     {
-        //
+        // $project = Project::find($id);
+        $project->delete();
+        return redirect()->route('admin.dashboard');
     }
 }

4dc7f7c8f1416345256ab574daf7648149de5b75
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-06 12:44:31 +0100

    Feat: implement project delete

--- resources/views/partials/library.blade.php
+++ resources/views/partials/library.blade.php
@@ -1,8 +1,8 @@
 <div class="container">
     <div class="row justify-content-center gap-3">
-        @foreach ($projects as $project)
-            <div class="col d-flex justify-content-center">
-                <div class="card h-100 w-100 m-2 glass" style="width: 18rem;">
+        <div class="d-flex justify-content-center flex-wrap">
+            @foreach ($projects as $project)
+                <div class="card w-100 m-2 glass" style="width: 18rem;">
                     <div class="card-img-top-container d-flex align-items-center justify-content-center">
                         <img src="{{ $project->image }}" class="card-img-top" alt="...">
                     </div>
@@ -15,17 +15,55 @@
                             href="{{ $project->url }}" target="_blank"><i class="fa-brands fa-github fs-2"></i>
                         </a>
 
+
+
                         @if (Route::getCurrentRoute()->uri == 'admin')
                             <a href="{{ route('admin.projects.edit', $project->id) }}"
                                 class="btn btn-warning me-3 w-25 d-flex justify-content-center align-items-center"><i
                                     class="fa-solid fa-pen fs-5"></i></a>
-                            <a href=""
-                                class="btn btn-danger me-3 w-25 d-flex justify-content-center align-items-center"><i
-                                    class="fa-solid fa-trash-can fs-5"></i></a>
+
+                            <!-- Button trigger modal -->
+                            <button type="button"
+                                class="btn btn-danger me-3 w-25 d-flex justify-content-center align-items-center"
+                                data-bs-toggle="modal" data-bs-target="#{{ $project->id }}">
+                                <i class="fa-solid fa-trash-can fs-5"></i>
+                            </button>
                         @endif
                     </div>
+
                 </div>
-            </div>
-        @endforeach
+                <form action="{{ route('admin.projects.destroy', $project) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="{{ $project->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content text-dark rounded-0">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="warningTitle">WARNING</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to delete {{ $project->name }}?</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary rounded-0 fw-bold"
+                                        data-bs-dismiss="modal">DISCARD</button>
+                                    <input id="deleteBtn" class="btn btn-danger rounded-0 fw-bold" type="submit"
+                                        value="DELETE">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            @endforeach
+        </div>
+
+
+
     </div>
 </div>

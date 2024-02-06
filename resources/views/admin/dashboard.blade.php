4ce0064d13daccf6665c99ecf8c76f4f2dcdd268
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-06 01:16:25 +0100

    Add library and create button

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,4 +20,13 @@
             </div>
         </div>
     </div>
+
+    <div class="my-5">
+        @include('partials.library')
+    </div>
+
+    <a id="add-project" class="glass border-0 text-black btn btn-primary d-flex align-items-center justify-content-center"
+        href="{{ route('admin.projects.create') }}">
+        <i class="fa-solid fa-plus fs-3"></i>
+    </a>
 @endsection
